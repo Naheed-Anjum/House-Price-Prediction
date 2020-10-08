3f73007807f0a290bc7c13b274d1620719779832
Naheed-Anjum <41262580+Naheed-Anjum@users.noreply.github.com>
2020-10-08 21:34:36 +0530

    Add files via upload

---
+++ 2. Boruta_FeatureSelectionMethod.R
@@ -0,0 +1,38 @@
+#Creating the Replica of original Dataset
+TrainDs_boruta <- TrainDs
+
+#install.packages("Boruta")
+library(Boruta)
+
+#convert the categorical variables into factor data type.
+for(i in 2:ncol(TrainDs_boruta)){
+  if (!is.numeric(TrainDs_boruta[,i]))
+  {
+    TrainDs_boruta[,i] <- data.frame(apply(TrainDs_boruta[i], 2, as.factor))
+    #print('Catogorical')
+  }
+}
+
+#Checking one of the categorical variable data type
+typeof(TrainDs_boruta$MSZoning)
+ 
+#Applying Boruta on Train Dataset
+set.seed(123)
+boruta.train <- Boruta(SalePrice~.-Id, data = TrainDs_boruta, doTrace = 2)
+print(boruta.train)
+
+#Taking decision on tentative attributes.  
+#The tentative attributes will be classified as confirmed or rejected by comparing the median Z score 
+#of the attributes with the median Z score of the best shadow attribute. 
+final.boruta <- TentativeRoughFix(boruta.train)
+print(final.boruta)
+
+#Checking the confirmed attributes.
+getSelectedAttributes(final.boruta, withTentative = F)
+
+#creating a data frame of the final result derived from Boruta.
+boruta.df <- attStats(final.boruta)
+class(boruta.df)
+ 
+print(boruta.df)
+
